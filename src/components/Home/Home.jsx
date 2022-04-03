cd3fa7e0febeba1f19c4c6423eb938a2041b71d4
Mikhail <mikhailkhudoshin@gmail.com>
2022-04-03 20:28:12 +0400

    feat: add header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,16 @@ import './Home.css';
 const Home = (props) => {
     return (
         <>
-
+            <section className="header">
+                <div className="container">
+                    <h1 className="header_title">Everything You Love About Coffee</h1>
+                    <div className="coffe_wrapper">
+                        <img className="coffe_hr" src="/assets/img-coffe/coffe-white.svg" alt="coffe" />
+                    </div>
+                    <h2 className="header_subtitle">We makes every day full of energy and taste <br /> Want to try our beans?</h2>
+                    <button className="header_btn">More</button>
+                </div>
+            </section>
         </>
     )
 }
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -9,16 +9,20 @@ const Navbar = (props) => {
                 <div className="container">
                     <ul className="navbar_list">
                         <li className="navbar_item">
-                            <NavLink to="/" className="nav-link">
+                            <NavLink to="/" className="navbar_link">
                                 <img className="navbar_logo" src="/assets/Logo.svg" alt="Logo"/>
-                                Coffe house
+                                <span>Coffe house</span>
                             </NavLink>
                         </li>
-                        <li className="navbar-item">
-                            <NavLink to="/our-coffe" className="nav-link">Our coffe</NavLink>
+                        <li className="navbar_item">
+                            <NavLink to="/our-coffe" className="navbar_link">
+                                <span>Our coffe</span>
+                            </NavLink>
                         </li>
-                        <li className="navbar-item">
-                            <NavLink to="/for-your-pleasure" className="nav-link">For your pleasure</NavLink>
+                        <li className="navbar_item">
+                            <NavLink to="/for-your-pleasure" className="navbar_link">
+                                <span>For your pleasure</span>
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
